d48904190ca01a1e572eb482c1c97ddc6109c684
JBernal <j.bernal@upm.es>
2017-09-25 00:10:48 +0200

    Create test Flyweight

---
+++ src/test/java/es/upm/miw/pd/flyweight/FlyweightTest.java
@@ -0,0 +1,27 @@
+package es.upm.miw.pd.flyweight;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FlyweightTest {
+    Factory factory;
+
+    @Before
+    public void before() {
+        factory = new Factory();
+    }
+
+    @Test
+    public void testSameObject() {
+        assertSame(factory.get("ocho"), factory.get("ocho"));
+    }
+    
+    @Test
+    public void testOperation() {
+        assertEquals("--ocho--", factory.get("ocho").operation("--"));
+    }
+    
+}
