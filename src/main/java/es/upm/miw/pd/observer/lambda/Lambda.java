2ce8856ffb1575e876a0e93599e9b1a4daaaefaa
jbernal <miw@etsisi.upm.es>
2019-08-21 10:27:05 +0200

    refactoring publisher & test

--- src/main/java/es/upm/miw/pd/observer/lambda/Lambda.java
+++ src/main/java/es/upm/miw/pd/observer/lambda/Lambda.java
@@ -10,34 +10,15 @@ import java.util.function.Supplier;
 
 public class Lambda {
     public Consumer<String> functionConsumer() { // accept(T)
-        return msg -> LogManager.getLogger(this.getClass()).info("Consumer: " + msg);
-    }
-
-    public Consumer<String> functionConsumer2() { // accept(T)
-        return (String msg) -> LogManager.getLogger(this.getClass()).info("Consumer: " + msg);
+        return msg -> LogManager.getLogger(this.getClass()).info("Consumer: {}", () -> msg);
     }
 
     public Function<String, Integer> function() { // apply(T):R
-        return msg -> Integer.parseInt(msg);
-    }
-
-    public Function<String, Integer> function2() { // apply(T):R
-        return msg -> {
-            msg.concat("...Concat");
-            return Integer.parseInt(msg);
-        };
-    }
-
-    public Function<String, Integer> function3() { // apply(T):R
-        return Integer::new;
+        return Integer::parseInt;
     }
 
     public Predicate<String> functionPredicate() { // test(T):boolean
         return "two"::equals;
-    }
-
-    public Predicate<String> functionPredicate2() { // test(T):boolean
-        return msg -> "two".equals(msg);
     }
 
     public Supplier<String> functionSupplier() { // get(): T
