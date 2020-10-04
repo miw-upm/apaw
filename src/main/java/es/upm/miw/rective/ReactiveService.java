9ce56e826dfbd3d9f788342b0cac2b4530f4867a
jbernal <jbernal@etsisi.upm.es>
2020-10-04 15:05:16 +0200

    improved reactive examples

--- src/main/java/es/upm/miw/rective/ReactiveService.java
+++ src/main/java/es/upm/miw/rective/ReactiveService.java
@@ -2,6 +2,7 @@ package es.upm.miw.rective;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
 import java.util.Arrays;
 import java.util.stream.Stream;
@@ -30,10 +31,6 @@ public class ReactiveService {
         return Flux.fromStream(stream);
     }
 
-    public Flux<String> filter(Flux<String> flux, String... values) {
-        return flux.filter(Arrays.asList(values)::contains);
-    }
-
     public Flux<String> concat(Flux<String> flux1, Flux<String> flux2) {
         return Flux.concat(flux1, flux2);
     }
@@ -42,12 +39,44 @@ public class ReactiveService {
         return Flux.merge(flux1, flux2);
     }
 
-    public Flux<String> errorIfEmpty(Flux<String> flux) {
-        return flux.switchIfEmpty(Flux.error(new RuntimeException("empty")));
+    public Flux<String> filter(Flux<String> flux, String... values) {
+        return flux.filter(Arrays.asList(values)::contains);
+    }
+
+    public Flux<Tuple2<String, Integer>> zip(Flux<String> flux1, Flux<Integer> flux2) {
+        return Flux.zip(flux1, flux2);
     }
 
     public Flux<String> zeroIfEmpty(Flux<String> flux) {
         return flux.defaultIfEmpty("0");
+    }
+
+    public Flux<String> switchIfEmpty(Flux<String> flux) {
+        return flux.switchIfEmpty(Flux.just("0", "1", "2"));
+    }
+
+    public Flux<String> errorIfEmpty(Flux<String> flux) {
+        return flux.switchIfEmpty(Flux.error(new RuntimeException("empty")));
+    }
+
+    public Mono<Void> and(Mono<String> mono, Flux<Integer> flux) {
+        return mono.and(flux);
+    }
+
+    public Mono<Void> when(Flux<String> flux1, Flux<Integer> flux2) {
+        return Mono.when(flux1, flux2);
+    }
+
+    public Mono<Void> thenEmpty(Mono<String> mono) {
+        return mono.then();
+    }
+
+    public Mono<String> thenMono(Mono<String> mono1, Mono<String> mono2) {
+        return mono1.then(mono2);
+    }
+
+    public Flux<String> thenFlux(Mono<String> mono, Flux<String> flux) {
+        return mono.thenMany(flux);
     }
 
     public Mono<Integer> sum(Flux<Integer> flux) {
--- src/test/java/es/upm/miw/reactive/ReactiveServiceTest.java
+++ src/test/java/es/upm/miw/reactive/ReactiveServiceTest.java
@@ -50,45 +50,10 @@ class ReactiveServiceTest {
     }
 
     @Test
-    void testSum() {
-        StepVerifier
-                .create(new ReactiveService().sum(Flux.just(1, 2, 3)))
-                .expectNext(6)
-                .expectComplete()
-                .verify();
-    }
-
-    @Test
-    void testSumEmpty() {
-        StepVerifier
-                .create(new ReactiveService().sum(Flux.empty()))
-                .expectNext(0)
-                .expectComplete()
-                .verify();
-    }
-
-    @Test
-    void testSumError() {
-        StepVerifier
-                .create(new ReactiveService().sum(Flux.error(new RuntimeException("mono-error"))))
-                .expectError()
-                .verify();
-    }
-
-    @Test
     void testFromStream() {
         StepVerifier
                 .create(new ReactiveService().fromStream(Stream.of("0", "1")))
                 .expectNext("0", "1")
-                .expectComplete()
-                .verify();
-    }
-
-    @Test
-    void testFilter() {
-        StepVerifier
-                .create(new ReactiveService().filter(Flux.just("0", "1", "2"), "1"))
-                .expectNext("1")
                 .expectComplete()
                 .verify();
     }
@@ -114,13 +79,28 @@ class ReactiveServiceTest {
                 .verify();
     }
 
+
     @Test
-    void testErrorIfEmpty() {
+    void testFilter() {
         StepVerifier
-                .create(new ReactiveService().errorIfEmpty(Flux.empty()))
-                .expectError()
+                .create(new ReactiveService().filter(Flux.just("0", "1", "2"), "1"))
+                .expectNext("1")
+                .expectComplete()
                 .verify();
     }
+
+
+    @Test
+    void testZip() {
+        StepVerifier
+                .create(new ReactiveService().zip(Flux.just("0", "1", "2"), Flux.just(0, 1, 2)))
+                .expectNextMatches(tuple -> "0".equals(tuple.getT1()) & 0 == tuple.getT2())
+                .expectNextCount(2)
+                .expectComplete()
+                .verify();
+
+    }
+
 
     @Test
     void testZeroIfEmpty() {
@@ -130,5 +110,94 @@ class ReactiveServiceTest {
                 .expectComplete()
                 .verify();
     }
+
+    @Test
+    void testSwitchIfEmpty() {
+        StepVerifier
+                .create(new ReactiveService().switchIfEmpty(Flux.empty()))
+                .expectNext("0")
+                .expectNext("1")
+                .expectNext("2")
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    void testErrorIfEmpty() {
+        StepVerifier
+                .create(new ReactiveService().errorIfEmpty(Flux.empty()))
+                .expectError()
+                .verify();
+    }
+
+    @Test
+    void testAnd() {
+        StepVerifier
+                .create(new ReactiveService().and(Mono.just("1"), Flux.just(0, 1)))
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    void testWhen() {
+        StepVerifier
+                .create(new ReactiveService().when(Flux.just("0", "1"), Flux.just(0, 1)))
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    void testThenEmpty() {
+        StepVerifier
+                .create(new ReactiveService().thenEmpty(Mono.just("1")))
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    void testThenMono() {
+        StepVerifier
+                .create(new ReactiveService().thenMono(Mono.just("1"), Mono.just("1")))
+                .expectNext("1")
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    void testThenFlux() {
+        StepVerifier
+                .create(new ReactiveService().thenFlux(Mono.just("1"), Flux.just("0", "1")))
+                .expectNext("0")
+                .expectNext("1")
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    void testSum() {
+        StepVerifier
+                .create(new ReactiveService().sum(Flux.just(1, 2, 3)))
+                .expectNext(6)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    void testSumEmpty() {
+        StepVerifier
+                .create(new ReactiveService().sum(Flux.empty()))
+                .expectNext(0)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    void testSumError() {
+        StepVerifier
+                .create(new ReactiveService().sum(Flux.error(new RuntimeException("mono-error"))))
+                .expectError()
+                .verify();
+    }
+
 
 }
