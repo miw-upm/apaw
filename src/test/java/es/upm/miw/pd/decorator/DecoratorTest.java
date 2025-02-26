4462f11a05b3baabd5a9b0a60bfc420cd2348899
JBernal <j.bernal@upm.es>
2025-02-26 11:22:19 +0100

    updated code

--- src/test/java/es/upm/miw/pd/decorator/DecoratorTest.java
+++ src/test/java/es/upm/miw/pd/decorator/DecoratorTest.java
@@ -8,13 +8,13 @@ class DecoratorTest {
     @Test
     void testDecoratorA() {
         Component component = new Component1();
-        LogManager.getLogger(this.getClass().getName()).info("\n" + new DecoratorA(component).view());
+        LogManager.getLogger(this.getClass().getName()).info(()->"\n" + new DecoratorA(component).view());
     }
 
     @Test
     void testDecoratorB() {
         Component component = new Component1();
-        LogManager.getLogger(this.getClass().getName()).info("\n" + new DecoratorB(component).view());
+        LogManager.getLogger(this.getClass().getName()).info(()->"\n" + new DecoratorB(component).view());
 
     }
 
@@ -22,6 +22,6 @@ class DecoratorTest {
     void testDecoratorAAndB() {
         Component component = new Component1();
         Component da = new DecoratorA(component);
-        LogManager.getLogger(this.getClass().getName()).info("\n" + new DecoratorB(da).view());
+        LogManager.getLogger(this.getClass().getName()).info(()->"\n" + new DecoratorB(da).view());
     }
 }
